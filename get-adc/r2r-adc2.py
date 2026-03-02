7fa931068f22f30acbd67741a9c5b6535fea5e73
Kredos <zel7dan@gmail.com>
2026-03-02 12:19:05 +0300

    adc2

--- get-adc/r2r-adc2.py
+++ get-adc/r2r-adc2.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import time
+
 class R2R_ADC:
     def __init__(self, dynamic_range, compare_time = 0.007, verbose = False):
         self.dynamic_range = dynamic_range
@@ -12,21 +13,25 @@ class R2R_ADC:
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.bits_gpio, GPIO.OUT, initial = 0)
         GPIO.setup(self.comp_gpio, GPIO.IN)
-        
-    def sequential_counting_adc(self):
-        v=0
-        while True:
+    def successive_approximation_adc(self):
+        vr=255
+        vl=0
+        k=[int(bit) for bit in bin(int(vl))[2:].zfill(8)]
+        GPIO.output(self.bits_gpio , k)
+        time.sleep(1)
+        while vl < vr-0.1:
+            v=(vl+vr)/2
+            time.sleep(0.1)
+            k=[int(bit) for bit in bin(int(v))[2:].zfill(8)]
+            GPIO.output(self.bits_gpio , k)
             if not GPIO.input(self.comp_gpio):
-                print(v*self.dynamic_range/255,GPIO.input(self.comp_gpio))
-                v+=1
-                k=[int(bit) for bit in bin(v)[2:].zfill(8)]
-                GPIO.output(self.bits_gpio , k)
+                vl=v
             else:
-                print(v*self.dynamic_range/255,GPIO.input(self.comp_gpio))
-                v-=1
-                k=[int(bit) for bit in bin(v)[2:].zfill(8)]
-                GPIO.output(self.bits_gpio , k)
-            time.sleep(0.005)
+                vr=v
+        
+        return vr
+    def get_sar_voltage(self):
+        print(self.successive_approximation_adc(), '================================')
     def deinit(self):
         GPIO.output(self.bits_gpio , 0)
         GPIO.cleanup()
@@ -35,7 +40,8 @@ try:
     dac = R2R_ADC(3.2, True)
     while True:
         try:
-            dac.sequential_counting_adc()
+            dac.get_sar_voltage()
+            time.sleep(3)
         except ValueError:
             print("Вы ввели не число. Попробуйте ещё раз\n")
 
